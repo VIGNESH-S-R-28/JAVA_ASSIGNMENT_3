f5045e49b91bb1afe065cc16f81c965be3b94143
VIGNESH RAJENDRAN <89457100+VIGNESH-S-R-28@users.noreply.github.com>
2021-09-19 09:51:06 +0530

    ADDITION

---
+++ ADDITION_PROGRAM.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ADDITION_PROGRAM {
+
+	
+		static String sum = "Sum of A and B = " ;
+
+		public static void main(String[] args) {
+
+			float A, B;
+
+			@SuppressWarnings("resource")
+			Scanner sc= new Scanner(System.in); 
+
+
+			System.out.print("Enter A value = ");
+			A = sc.nextInt();
+
+
+			System.out.print("Enter B value = ");
+		    B = sc.nextInt(); 
+
+		    float C = A + B;
+
+
+		    System.out.println(sum+ C);
+
+	}
+
+}
