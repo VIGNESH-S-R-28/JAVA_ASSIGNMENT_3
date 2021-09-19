61260a4df59a2f792e397809a9a53917888f3f50
VIGNESH RAJENDRAN <89457100+VIGNESH-S-R-28@users.noreply.github.com>
2021-09-19 09:51:28 +0530

    MISSING CODE

---
+++ MISSING_CODE.java
@@ -0,0 +1,28 @@
+
+public class MISSING_CODE {
+	class KCG
+
+	{
+
+	static void cse()
+
+	{
+
+	System.out.println("Welcome to CES");
+
+	}
+
+	public static void main(String[] args)
+
+	{
+		KCG.cse();
+
+
+	}
+
+	}
+
+}
+
+
+
